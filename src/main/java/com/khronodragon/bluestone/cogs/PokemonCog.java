3bc7462d9d78b7f9315a686f465c23a84ab531ba
kdrag0n <dragon@khronodragon.com>
2017-06-24 09:58:54 -0700

    Start work on Pokémon cog

---
+++ src/main/java/com/khronodragon/bluestone/cogs/PokemonCog.java
@@ -0,0 +1,26 @@
+package com.khronodragon.bluestone.cogs;
+
+import com.khronodragon.bluestone.Bot;
+import com.khronodragon.bluestone.Cog;
+import com.khronodragon.bluestone.Context;
+import com.khronodragon.bluestone.annotations.Command;
+
+public class PokemonCog extends Cog {
+    public PokemonCog(Bot bot) {
+        super(bot);
+    }
+
+    public String getName() {
+        return "Pokémon";
+    }
+
+    public String getDescription() {
+        return "Gotta catch 'em all!";
+    }
+
+    @Command(name = "pokémon", desc = "Get the info on a Pokémon!",
+            aliases = {"pokemon", "pokèmon", "pokedex", "pokédex"}, usage = "{name or id}?")
+    public void command(Context ctx) {
+
+    }
+}
