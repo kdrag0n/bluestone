dbadce0343610959f2e99608da50b576a96ab473
kdrag0n <dragon@khronodragon.com>
2017-06-25 09:06:09 -0700

    Finish LanguageTool-based correction, remove massive dictionary

--- src/main/java/com/khronodragon/bluestone/cogs/KewlCog.java
+++ src/main/java/com/khronodragon/bluestone/cogs/KewlCog.java
@@ -14,10 +14,14 @@ import org.languagetool.language.AmericanEnglish;
 import org.languagetool.rules.RuleMatch;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class KewlCog extends Cog {
     private static final Logger logger = LogManager.getLogger(KewlCog.class);
+    private static final Pattern DATE_WEEKDAY_PATTERN = Pattern.compile("^The date [0-9 a-zA-Z]+ is not a ([MTWFS][a-z]+), but a ([MTWFS][a-z]+)\\.$");
     private static final Language language = new AmericanEnglish();
     private final ThreadLocal<JLanguageTool> langTool = ThreadLocal.withInitial(() -> {
         logger.info("Creating new LanguageTool instance...");
@@ -43,15 +47,31 @@ public class KewlCog extends Cog {
             ctx.send(":warning: I need something to correct!").queue();
             return;
         }
+        ctx.channel.sendTyping().queue();
         final String text = ctx.rawArgs;
         StringBuilder result = new StringBuilder(text);
 
         List<RuleMatch> matches = langTool.get().check(text);
-        for (RuleMatch match: matches) {
-            if (match.getSuggestedReplacements().size() > 0)
-                result.replace(match.getFromPos(), match.getToPos(), match.getSuggestedReplacements().get(0));
-        }
+        Collections.reverse(matches);
 
-        ctx.send("Result: `" + result + "`").queue();
+        for (RuleMatch match: matches) {
+            logger.info("rmatch {} - from {} to {} rep {}", match, match.getFromPos(), match.getToPos(), match.getSuggestedReplacements());
+
+            if (match.getSuggestedReplacements().size() > 0) {
+                result.replace(match.getFromPos(), match.getToPos(), match.getSuggestedReplacements().get(0));
+            } else if (match.getRule().getId().equals("DATE_WEEKDAY")) {
+                Matcher m = DATE_WEEKDAY_PATTERN.matcher(match.getMessage());
+                if (!m.find()) continue;
+
+                String wrongWeekday = m.group(1);
+                String correctWeekday = m.group(2);
+
+                result.replace(match.getFromPos(), match.getToPos(),
+                        result.substring(match.getFromPos(), match.getToPos()).replace(wrongWeekday, correctWeekday));
+            }
+        }
+        String finalResult = result.toString().replace(".M..", "M.");
+
+        ctx.send("Result: `" + finalResult + "`").queue();
     }
 }
