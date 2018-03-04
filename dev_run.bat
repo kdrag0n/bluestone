4a4e3805480bc4c1ea86f03a43a4e983572070e9
kdrag0n <dragon@khronodragon.com>
2018-03-04 14:41:09 -0800

    [build] remove dev_run scripts, use gradle run instead

--- dev_run.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-java -Xms10M -Xmx1G -XX:+UseG1GC -server -XX:+CMSClassUnloadingEnabled -XX:+AggressiveOpts -XX:MaxGCPauseMillis=2500 -Xverify:none -cp build/classes/java/main;build/libs/bluestone-1.0-SNAPSHOT.jar;build/libs/bluestone-1.0-SNAPSHOT-all.jar com.khronodragon.bluestone.Start
--- dev_run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -Xms10M -Xmx500M -XX:+UseG1GC -server -XX:+CMSClassUnloadingEnabled -XX:+AggressiveOpts -XX:MaxGCPauseMillis=2500 -Xverify:none -cp build/classes/java/main:build/libs/bluestone-1.0-SNAPSHOT.jar:build/libs/bluestone-1.0-SNAPSHOT-all.jar -Dapple.awt.UIElement=true com.khronodragon.bluestone.Start
