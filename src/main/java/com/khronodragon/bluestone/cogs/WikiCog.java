8ba798984057a6840faa6203e2a87b71f58bf7e7
kdrag0n <dragon@khronodragon.com>
2018-03-25 14:30:34 -0700

    Clean up wikipedia

--- src/main/java/com/khronodragon/bluestone/cogs/WikiCog.java
+++ src/main/java/com/khronodragon/bluestone/cogs/WikiCog.java
@@ -1,23 +1,17 @@
 package com.khronodragon.bluestone.cogs;
 
-import com.censhare.db.iindex.IDSetTrie;
-import com.j256.ormlite.dao.Dao;
 import com.khronodragon.bluestone.Bot;
 import com.khronodragon.bluestone.Cog;
 import com.khronodragon.bluestone.Context;
 import com.khronodragon.bluestone.annotations.Command;
-import com.khronodragon.bluestone.annotations.EventHandler;
 import net.dv8tion.jda.core.EmbedBuilder;
 import okhttp3.Request;
-import okhttp3.RequestBody;
 import org.json.JSONArray;
-import org.json.JSONObject;
 
 /**
- * Wikipedia Cog.
- * @author  Beefywhale
+ * Wikipedia cog
+ * @author Beefywhale
  */
-
 public class WikiCog extends Cog {
     public WikiCog(Bot bot) {
         super(bot);
@@ -28,25 +22,26 @@ public class WikiCog extends Cog {
     }
 
     public String getDescription() {
-        return "Get a wikipedia page.";
+        return "Wikipedia, the online encyclopedia.";
     }
 
-    @Command(name = "wiki",
-            desc = "Get a specific wikipedia page.",
-            usage = "[topic]", thread = true, aliases = {"wikipedia"})
-    public void wikiAfk(Context ctx) {
+    @Command(name = "wiki", desc = "Get a Wikipedia page.",
+            usage = "[topic]", aliases = {"wikipedia"})
+    public void cmdWiki(Context ctx) {
         if (ctx.args.empty) {
             ctx.fail("You need some search terms!");
             return;
         }
+
         EmbedBuilder emb = new EmbedBuilder()
                 .setColor(randomColor())
                 .setAuthor("Wikipedia", "https://wikipedia.com/",
                 "https://upload.wikimedia.org/wikipedia/en/thumb/8/80/Wikipedia-logo-v2.svg/1122px-Wikipedia-logo-v2.svg.png");
 
         Bot.http.newCall(new Request.Builder()
-                .url("https://en.wikipedia.org/w/api.php?action=opensearch&search="+ctx.rawArgs)
+                .url("https://en.wikipedia.org/w/api.php?action=opensearch&search=" + ctx.rawArgs)
                 .build()).enqueue(Bot.callback(response -> {
+
             if (!response.isSuccessful()) {
                 emb.setDescription("⚠ Failed to get results from Wikipedia.");
                 ctx.send(emb.build()).queue();
