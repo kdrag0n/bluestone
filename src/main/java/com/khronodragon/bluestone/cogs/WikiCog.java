afa63c191b11ec5a4016ff6d616040778fe7efd7
Jonathan <jonathan@integritypts.com>
2018-03-25 02:02:00 -0500

    Cleanup
    Cleaned and removed stuff.

--- src/main/java/com/khronodragon/bluestone/cogs/WikiCog.java
+++ src/main/java/com/khronodragon/bluestone/cogs/WikiCog.java
@@ -1,7 +1,5 @@
 package com.khronodragon.bluestone.cogs;
 
-//cog made by Beefywhale
-
 import com.censhare.db.iindex.IDSetTrie;
 import com.j256.ormlite.dao.Dao;
 import com.khronodragon.bluestone.Bot;
@@ -9,24 +7,18 @@ import com.khronodragon.bluestone.Cog;
 import com.khronodragon.bluestone.Context;
 import com.khronodragon.bluestone.annotations.Command;
 import com.khronodragon.bluestone.annotations.EventHandler;
-import com.khronodragon.bluestone.sql.AfkMessage;
-import gnu.trove.set.TLongSet;
-import gnu.trove.set.hash.TLongHashSet;
 import net.dv8tion.jda.core.EmbedBuilder;
-import net.dv8tion.jda.core.entities.User;
-import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 import okhttp3.Request;
 import okhttp3.RequestBody;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.net.SocketTimeoutException;
+/**
+ * Wikipedia Cog.
+ * @author  Beefywhale
+ */
 
 public class WikiCog extends Cog {
-    private static final Logger logger = LogManager.getLogger(StatReporterCog.class);
-
     public WikiCog(Bot bot) {
         super(bot);
     }
@@ -56,7 +48,6 @@ public class WikiCog extends Cog {
                 .url("https://en.wikipedia.org/w/api.php?action=opensearch&search="+ctx.rawArgs)
                 .build()).enqueue(Bot.callback(response -> {
             if (!response.isSuccessful()) {
-                logger.warn("Wikipedia Bad response: {} {}", response.code(), response.message());
                 emb.setDescription("⚠ Failed to get results from Wikipedia.");
                 ctx.send(emb.build()).queue();
                 return;
@@ -68,12 +59,7 @@ public class WikiCog extends Cog {
             ctx.send(emb.build()).queue();
 
             response.body().close();
-        }, e -> {
-            if (e instanceof SocketTimeoutException)
-                logger.error("Wikipedia: timeout");
-            else
-                logger.error("Wikipedia failed", e);
-        }));
+        }, e -> {}));
     }
 
 }
